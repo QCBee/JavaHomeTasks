8d1647e56cc3c09e55b094110c5d083b909eddbb
oliashchevska <Pala2Dox1991%github>
2019-02-08 18:06:12 +0200

    1) Added files in order to ignore some dir 2) Added logic for calculating the sum of purchase

--- src/com/TaskPractice/Task_Practice.java
+++ src/com/TaskPractice/Task_Practice.java
@@ -1,4 +1,25 @@
 package com.TaskPractice;
 
+import java.util.Scanner;
+
 public class Task_Practice {
+    public static void main (String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите стоимость 1 кг конфет и нажмите Enter:");
+        float CandyPrice = console.nextFloat();
+
+        System.out.println("Введите стоимость 1 кг печенья и нажмите Enter:");
+
+        float CookyPrice = console.nextFloat();
+        float fistPurchase = calculateFirstPurchase (CandyPrice, CookyPrice);
+
+        System.out.println("Стоимость покупки " + fistPurchase);
+
+    }
+
+    public static float calculateFirstPurchase (float CandyPrice, float CookyPrice) {
+        return Math.round(3*(1.8f*CandyPrice + 2f*CookyPrice));
+    }
+
+
 }
